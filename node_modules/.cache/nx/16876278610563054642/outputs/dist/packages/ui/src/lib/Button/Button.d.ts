5f3d95de370c6f5c22f933763b5eedac4c0a4d34
Igor Alpert <ialpert@focusbrands.com>
2023-08-16 14:44:53 -0400

    cleanup

--- node_modules/.cache/nx/16876278610563054642/outputs/dist/packages/ui/src/lib/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-}
-export declare function Button({ children, ...props }: ButtonProps): import("react/jsx-runtime").JSX.Element;
-export default Button;
