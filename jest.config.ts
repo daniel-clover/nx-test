769995f80d52dbfce7bd2ac0ace9a94fa91f2597
Daniel Donaldson <daniel.donaldson@clover.com>
2023-08-18 09:04:01 -0700

    add jest-dom, still failing

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,7 @@
 import { getJestProjects } from '@nx/jest';
+import '@testing-library/jest-dom';
 
 export default {
   projects: getJestProjects(),
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
 };
