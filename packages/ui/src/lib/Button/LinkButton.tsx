3bdd45145f2c3b7aa571790dbcac15ec253624d1
Igor Alpert <igor.alpert@canoo.com>
2023-08-17 21:09:51 -0400

    types fix

--- packages/ui/src/lib/Button/LinkButton.tsx
+++ packages/ui/src/lib/Button/LinkButton.tsx
@@ -1,14 +1,14 @@
-import React, { forwardRef } from 'react';
-import { Link, LinkProps } from 'react-router-dom';
-import { ButtonVariant } from './Button';
-import cx from 'clsx';
-import styles from './styles.module.scss';
+import React, { forwardRef } from "react";
+import { Link } from "react-router-dom";
+import { ButtonVariant } from "./Button";
+import cx from "clsx";
+import styles from "./styles.module.scss";
 
 export type LinkButtonProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
   /** Defaults to 'primary' */
   variant?: ButtonVariant;
   /** sets width to 100% */
-  fullwidth?: boolean;
+  fullWidth?: boolean;
   /** increase font-size and padding */
   large?: boolean;
   /** if it is React Link the route to load */
@@ -54,43 +54,50 @@ export type LinkButtonProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
 </LinkButton>
  */
 export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(
-  ({ variant = 'primary', fullwidth, large, className, children, href, to, ...passThroughProps }, ref) => {
-    const linkProps: LinkProps = { to };
+  (
+    {
+      variant = "primary",
+      fullWidth,
+      large,
+      className,
+      children,
+      href,
+      to,
+      ...passThroughProps
+    },
+    ref
+  ) => {
     const isLink = href !== undefined && !to;
-    return (
-      <>
-        {isLink ? (
-          <a
-            ref={ref}
-            className={cx(
-              styles.button,
-              styles[variant],
-              fullwidth && styles.fullwidth,
-              large && styles.large,
-              className
-            )}
-            href={href}
-            {...passThroughProps}
-          >
-            {children}
-          </a>
-        ) : (
-          <Link
-            ref={ref}
-            className={cx(
-              styles.button,
-              styles[variant],
-              fullwidth && styles.fullwidth,
-              large && styles.large,
-              className
-            )}
-            {...linkProps}
-            {...passThroughProps}
-          >
-            {children}
-          </Link>
+    return isLink ? (
+      <a
+        ref={ref}
+        className={cx(
+          styles.button,
+          styles[variant],
+          fullWidth && styles.fullwidth,
+          large && styles.large,
+          className
         )}
-      </>
+        href={href}
+        {...passThroughProps}
+      >
+        {children}
+      </a>
+    ) : (
+      <Link
+        ref={ref}
+        to={to ?? ""}
+        className={cx(
+          styles.button,
+          styles[variant],
+          fullWidth && styles.fullwidth,
+          large && styles.large,
+          className
+        )}
+        {...passThroughProps}
+      >
+        {children}
+      </Link>
     );
   }
 );
